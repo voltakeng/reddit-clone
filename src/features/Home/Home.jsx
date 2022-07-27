0d4b5a0fff045b3ee259c0938abe3b85ba049e8a
voltagit <pitawat76@gmail.com>
2022-07-27 20:14:39 +0700

    renamed:    src/css/Card.css -> src/components/Card/Card.css 	renamed:    src/css/Avatar.css -> src/features/Avatar/Avatar.css 	renamed:    src/css/Comment.css -> src/features/Comment/Comment.css 	renamed:    src/css/Home.css -> src/features/Home/Home.css 	new file:   src/features/Home/Home.jsx 	renamed:    src/css/Post.css -> src/features/Post/Post.css 	new file:   src/features/Post/Post.jsx 	renamed:    src/css/PostLoading.css -> src/features/Post/PostLoading.css 	renamed:    src/css/Subreddits.css -> src/features/Post/PostLoading.jsx 	new file:   src/features/Subreddits/Subreddits.css

---
+++ src/features/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "../Post/Post";
+import "./Home.css"; 
+
+function Home() {
+    return (
+        <>
+            <Post /> 
+        </>
+    );
+}
+
+export default Home; 
---
+++ src/features/Post/Post.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Post.css"; 
+
+function Post() {
+    return (
+        <article>
+            <Card>
+            </Card>
+        </article>
+    )
+}
+
+export default Post; 

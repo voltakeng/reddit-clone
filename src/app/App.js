bd5b14a404914d13e6e6d3cfdcb6f08955c6e8cd
voltagit <pitawat76@gmail.com>
2022-07-27 16:00:20 +0700

    new file:   public/css/App.css 	new file:   public/css/Avatar.css 	new file:   public/css/Card.css 	new file:   public/css/Comment.css 	new file:   public/css/Header.css 	new file:   public/css/Home.css 	new file:   public/css/Post.css 	new file:   public/css/PostLoading.css 	new file:   public/css/Subreddits.css 	new file:   public/css/index.css 	new file:   public/css/main.791fefe6.chunk.css 	modified:   public/index.html 	modified:   src/app/App.js 	new file:   src/features/Header/Header.js 	modified:   src/index.js

---
+++ src/app/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Headers } from "../features/Header/Header";
+
+function App() {
+    return (
+        <Headers /> 
+    );
+};
+
+export default App; 
---
+++ src/features/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Headers() {
+    return (
+        <div>
+            <p>Hi</p>
+        </div>
+    )
+}
